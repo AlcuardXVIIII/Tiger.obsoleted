02925e85745e714643c1e75b8778c593fbe4745a
AssemblyCoffee <1686560661@qq.com>
2014-11-29 15:28:26 +0800

    prepare chap10

---
+++ chap10/util.c
@@ -0,0 +1,85 @@
+/*
+ * util.c - commonly used utility functions.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <assert.h>
+#include "util.h"
+
+static string simple_itoa(int n,int radix);
+
+void *checked_malloc(int len){
+  void *p = malloc(len);
+  if (!p) {
+    fprintf(stderr,"\nRan out of memory!\n");
+    exit(1);
+  }
+  return p;
+}
+
+string String(char *s){
+  string p = checked_malloc(strlen(s)+1);
+  strcpy(p,s);
+  return p;
+}
+
+U_boolList U_BoolList(bool head, U_boolList tail){
+  U_boolList list = checked_malloc(sizeof(*list));
+  list->head = head;
+  list->tail = tail;
+  return list;
+}
+
+string string_format(const string format,...){
+  string buf = checked_malloc(INITSIZE);
+  int bufSize = INITSIZE;
+  const char *slice = NULL;
+  char* curPos = format;
+  int size = 0,sliceSize=0;//size:size of
+  va_list ap;
+  va_start(ap,format);
+  while(*curPos){
+    if(*curPos=='%'){
+      curPos++;
+      if(*curPos=='d'){
+        slice = simple_itoa(va_arg(ap,int),10);
+      }else if(*curPos=='s'){
+        slice = va_arg(ap,const char*);
+      }else if(*curPos=='x'){
+        slice = simple_itoa(va_arg(ap,int),16);
+      }else{
+        assert(0);
+      }
+      sliceSize = strlen(slice);
+    }
+    else{
+      slice = curPos;
+      sliceSize = 1;
+    }
+    if(size+sliceSize>=bufSize){
+      string tmp = checked_malloc(size*2+1);
+      strncpy(tmp,buf,size);
+      buf = tmp;
+    }
+    strncpy(buf+size,slice,sliceSize);
+    size+=sliceSize;
+    curPos++;
+  }
+  va_end(ap);
+  return buf;
+}
+
+static string simple_itoa(int n,int radix){
+  string buf = checked_malloc(INITSIZE);
+  if(radix==10){
+    sprintf(buf,"%d",n);
+  }else if(radix==16){
+    sprintf(buf,"%x",n);
+  }else{
+    assert(0);
+  }
+  return buf;
+}
