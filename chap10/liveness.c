f85e09da68ff339279d557a76a6405b2c40f49ac
AssemblyCoffee <1686560661@qq.com>
2014-12-03 23:00:56 +0800

    revise return value of the function Live_liveness

--- chap10/liveness.c
+++ chap10/liveness.c
@@ -4,12 +4,29 @@
 #include "temp.h"
 #include "assem.h"
 #include "graph.h"
-#include "flowgraph.h"
 #include "liveness.h"
 
 static void enterLiveMap(G_table t,G_node flowNode,Temp_tempList temp){
-
+  G_enter(t,flowNode,temp);
 }
 static Temp_tempList lookupLiveMap(G_table t,G_node flownode){
+  return (Temp_tempList)G_look(t,flownode);
+}
+Live_moveList Live_MoveList(G_node src,G_node dst,Live_moveList tail){
+  Live_moveList live_moveList = checked_malloc(sizeof(*live_moveList));
+  live_moveList->src = src;
+  live_moveList->dst = dst;
+  live_moveList->tail = tail;
+  return live_moveList;
+}
+Temp_temp Live_gtemp(G_node n){
 
+}
+Live_graph Live_liveness(G_graph flow){
+  Live_graph live_graph = checked_malloc(sizeof(*live_graph));
+
+
+
+
+  return live_graph;
 }
--- chap10/liveness.h
+++ chap10/liveness.h
@@ -1,12 +1,13 @@
 typedef struct Live_moveList_ *Live_moveList;
+typedef struct Live_graph_ *Live_graph;
 struct Live_moveList_ {
-  G_node src,det;
+  G_node src,dst;
   Live_moveList tail;
 };
 Live_moveList Live_MoveList(G_node src,G_node dst,Live_moveList tail);
-struct List_graph{
+struct Live_graph_{
   G_graph graph;
   Live_moveList moves;
 };
 Temp_temp Live_gtemp(G_node n);
-struct Live_graph Live_liveness(G_graph flow);
+Live_graph Live_liveness(G_graph flow);
