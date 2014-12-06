b50ef20043b36afacdb4b3e222c3b0eab97ebb1d
AssemblyCoffee <1686560661@qq.com>
2014-12-06 22:57:00 +0800

    the skeleton code of chap11

--- chap11/regallo.c
+++ chap11/regallo.c
@@ -9,8 +9,28 @@
 #include "color.h"
 #include "tree.h"
 #include "frame.h"
-#include "regalloc.h"
+#include "regallo.c"
 
-struct RA_result RA_regAlloc(F_frame h,AS_instrList il){
+RA_result RA_regAlloc(F_frame f,AS_instrList il){
+  G_graph g_graph = FG_AssemFlowGraph(il);
+  Live_graph live_graph = Live_liveness(g_graph);
+  g_graph = live_graph->graph;
+  Live_moveList Live_moveList = live_graph->moves;
+
+
+
+
+  Temp_map initial = NULL;
+  Temp_tempList regs = NULL;
+  COL_result col_result = COL_color(g_graph,initial,regs);
+  if(col_result->spills!=NULL){
+    //TODO
+    il = NULL;
+    return RA_regAlloc(f,il);
+  }
+  RA_result ra_result = checked_malloc(sizeof(*ra_result));
+  ra_result->coloring = col_result->coloring;
   //TODO
+  ra_result->il = il;
+  return ra_result;
 }
