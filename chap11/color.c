29f5a97aa8c9732a0ff3ff4b2d7be923a192da43
AssemblyCoffee <1686560661@qq.com>
2014-12-09 22:54:46 +0800

    update color.c&h

--- chap11/color.c
+++ chap11/color.c
@@ -9,22 +9,70 @@
 #include "liveness.h"
 #include "color.h"
 
-/*static G_nodeList precolored = NULL;
-static G_nodeList simplifyWorklist = NULL;
-static G_nodeList freezeWorklist = NULL;
-static G_nodeList spillWorklist = NULL;
-static G_nodeList spilledNodes  = NULL;
-static G_nodeList coalescedNodes = NULL;
-static G_nodeList coloredNodes = NULL;
-static G_nodeList selectStack = NULL;
+static G_nodeList2 precolored = NULL;
+static G_nodeList2 simplifyWorklist = NULL;
+static G_nodeList2 freezeWorklist = NULL;
+static G_nodeList2 spillWorklist = NULL;
+static G_nodeList2 spilledNodes  = NULL;
+static G_nodeList2 coalescedNodes = NULL;
+static G_nodeList2 coloredNodes = NULL;
+static G_nodeList2 selectStack = NULL;
 
-static Live_moveList coalescedMoves = NULL;
-static Live_moveList constraintMoves = NULL;
-static Live_moveList frozenMoves = NULL;
-static Live_moveList workListMoves = NULL;
-static Live_moveList activeMoves = NULL;
-*/
+static Live_moveList2 coalescedMoves = NULL;
+static Live_moveList2 constraintMoves = NULL;
+static Live_moveList2 frozenMoves = NULL;
+static Live_moveList2 workListMoves = NULL;
+static Live_moveList2 activeMoves = NULL;
 
+
+G_nodeList2 G_NodeList2(G_node node,G_nodeList2 pre,G_nodeList2 next){
+  G_nodeList2 g_nodeList2 = checked_malloc(sizeof(*g_nodeList2));
+  g_nodeList2->node = node;
+  g_nodeList2->kind = OTHER;
+  g_nodeList2->pre = pre;
+  g_nodeList2->next = next;
+  return g_nodeList2;
+}
+Live_moveList2 Live_MoveList2(G_node src,G_node dst,Live_moveList2 pre,Live_moveList2 next){
+  Live_moveList2 live_moveList2 = checked_malloc(sizeof(*live_moveList2));
+  live_moveList2->src = src;
+  live_moveList2->dst = dst;
+  live_moveList2->pre = pre;
+  live_moveList2->next = next;
+  return live_moveList2;
+}
+
+static setSimplify(G_nodeList2 g_nodeList2){
+  assert(g_nodeList2);
+  g_nodeList2->kind = SIMPLIFY;
+}
+static setSpill(G_nodeList2 g_nodeList2){
+  assert(g_nodeList2);
+  g_nodeList2->kind = SPILL;
+}
+static setFreeze(G_nodeList2 g_nodeList2){
+  assert(g_nodeList2);
+  g_nodeList2->kind = FREEZE;
+}
+static setOther(G_nodeList2 g_nodeList2){
+  assert(g_nodeList2);
+  g_nodeList2->kind = OTHER;
+}
+static setCoalesced(Live_moveList2 live_moveList2){
+  live_moveList2->kind = COALESCED;
+}
+static setConstraint(Live_moveList2 live_moveList2){
+  live_moveList2->kind = CONSTRAINT;
+}
+static setFrozen(Live_moveList2 live_moveList2){
+  live_moveList2->kind = FROZEN;
+}
+static setWorkList(Live_moveList2 live_moveList2){
+  live_moveList2->kind = WORKLIST;
+}
+static setActive(Live_moveList2 live_moveList2){
+  live_moveList2->kind = ACTIVE;
+}
 COL_result COL_color(G_graph ig,Temp_map inital,Temp_tempList regs){
   COL_result col_result = checked_malloc(sizeof(*col_result));
   //TODO
--- chap11/color.h
+++ chap11/color.h
@@ -3,7 +3,22 @@
  *             to determine register allocation.
  */
 typedef struct COL_result_ *COL_result;
+typedef struct G_nodeList2_ *G_nodeList2;
+typedef struct Live_moveList2_ *Live_moveList2;
+
 struct COL_result_ {Temp_map coloring; Temp_tempList spills;};
 COL_result COL_color(G_graph ig, Temp_map initial, Temp_tempList regs);
-
-
+struct G_nodeList2_{
+  G_node node;
+  G_nodeList2 pre;
+  G_nodeList2 next;
+  enum{SIMPLIFY,SPILL,FREEZE,OTHER}kind;
+};
+G_nodeList2 G_NodeList2(G_node node,G_nodeList2 pre,G_nodeList2 next);
+struct Live_moveList2_{
+  G_node src,dst;
+  enum{COALESCED,CONSTRAINT,FROZEN,WORKLIST,ACTIVE}kind;
+  Live_moveList2 pre;
+  Live_moveList2 next;
+};
+Live_moveList2 Live_MoveList2(G_node src,G_node dst,Live_moveList2 pre,Live_moveList2 next);
